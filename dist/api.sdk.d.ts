ff93d6df7cb0ec718f61dc6e50100d1867d959ba
Marek Kędzia <96777780+marekkedzia@users.noreply.github.com>
2024-10-08 20:14:26 +0200

    Delete dist directory

--- dist/api.sdk.d.ts
+++
@@ -1,6 +0,0 @@
-import { ApiClient } from "./api.client";
-import { ClientCredentials } from "./schemas/client.credentials";
-export declare class PlumberApiSDK {
-    protected apiClient: ApiClient;
-    constructor(clientCredentials: ClientCredentials);
-}
--- dist/schemas/client.credentials.d.ts
+++
@@ -1,6 +0,0 @@
-export type ClientCredentials = {
-    clientSecret: string;
-    clientId: string;
-    authHeader: string | string[];
-    url: string;
-};
